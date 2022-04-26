bf50a7055a3a3cbfdf9d72a710b9f9aa16dd0ecf
bubble <bubblegame061@outlook.it>
2022-04-26 23:25:57 +0200

    Box edits & Responsive

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -16,8 +16,11 @@ export default function Home() {
     <div>
 
      <Head>
-   
-    
+      <meta charset="UTF-8"/>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  	  <meta name="description" content="" />
+  	  <meta name="keywords" content=""/>
+      <title>Titolo | Home</title>
      </Head>
      
   
